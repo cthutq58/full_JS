66d5dc6d7d4f48d273607c34c07b8e4b2599fc87
Ser <cthutq58@gmail.com>
2020-01-29 02:08:43 +0300

    new end 06_lesson

--- 06_lesson/js.js
+++ 06_lesson/js.js
@@ -15,28 +15,29 @@ let btn = document.querySelectorAll('button'),
 
 // btn[0].addEventListener('mouseenter', () => 
 //     alert('You are over the 1st button!'));
-
-btn[0].addEventListener('click', event => {
+const handlerBtn0 = event => {
     console.log('the 1st button');
     let target = event.target;
     // target.style.display = "none";
     console.log('Here has been the ' + event.type + ' event on the ' + target);
-});
+};
+btn[0].addEventListener('click', handlerBtn0);
 
 wrap.style.backgroundColor = "red";
-
-wrap.addEventListener('click', event => {
+const handlerWrap = event => {
     console.log('wrapper of the 1st button');
     let target = event.target;
     console.log('Here has been the ' + event.type + ' event on the ' + target);
-});
+};
+wrap.addEventListener('click', handlerWrap);
 
-link.addEventListener('click', event => {
+const handlerLink = event => {
     let target = event.target;
     event.preventDefault();
     console.log('link');
     console.log('Here has been the ' + event.type + ' event on the ' + target);
-});
+};
+link.addEventListener('click', handlerLink);
 
-btn.forEach(item => 
-    item.addEventListener('mouseleave', () => console.log("Off!")));
+const handlerBtn = () => console.log("Off!");
+btn.forEach(item => item.addEventListener('mouseleave', handlerBtn));
