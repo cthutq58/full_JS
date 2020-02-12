be1e36d57d4d8624a063496fa342350ff7b27dda
Ser <cthutq58@gmail.com>
2020-02-12 18:47:02 +0300

    the end of 07_lesson

--- 07_lesson/js.js
+++ 07_lesson/js.js
@@ -24,36 +24,56 @@ let timerID = setTimeout(function log(i = 0, x = 5) {
 let btn = document.querySelector('.btn'),
     box = document.querySelector('.box');
 
-function myAmimation () {
-    let posX = 0, posY = 0,
+function myAmimation(msec = 17, cycles = 2) {
+    let posX = 0, posY = 0, size = 300,
         // start = Date.now();
         cnt = 0;
 
-    let t = setInterval(function(elem) {
+    let timer = setInterval(fnInterval, msec);
+
+    function fnInterval(fn = movePerimeter) {
         // let timePassed = Date.now() - start;
         // if (timePassed > 12000) {
-        if (++cnt > 1200) {
-            clearInterval(t);
+        if (++cnt > 4 * size * cycles) {
+            clearInterval(timer);
             return;
         }
-        move(elem);
-    }, 10, box);
+        fn();
+    }
 
-    function move(elem, x = 300) {
-        if(posX >= 0 && posX < x && posY <= 0) {
+    function movePerimeter(elem = box, x = size) {
+        if (posX >= 0 && posX < x && posY <= 0) {
             posX++;
-        } else if(posX >= x && posY < x && posY >= 0) {
+        } else if (posX >= x && posY < x && posY >= 0) {
             posY++;
-        } else if(posX > 0 && posX <= x && posY >= x) {
+        } else if (posX > 0 && posX <= x && posY >= x) {
             posX--;
-        } else if(posX <= 0 && posY <= x && posY > 0) {
+        } else if (posX <= 0 && posY <= x && posY > 0) {
             posY--;
         }
-        elem.style.left = posX +'px';
-        elem.style.top = posY +'px';
-    } 
-}   
+        elem.style.left = posX + 'px';
+        elem.style.top = posY + 'px';
+    }
+}
 
 btn.addEventListener('click', myAmimation);
 
+/* further => event deligation  */
 
+let btnBlock = document.querySelector('.btn-block'),
+    btns = document.getElementsByTagName('button');
+
+btnBlock.addEventListener('click', event => {
+    if (event.target && event.target.classList.contains('first'))
+        console.log("Hey, first!");
+});
+
+btnBlock.addEventListener('click', event => {
+    if (event.target && event.target.matches('button.second')) // like in Google
+        console.log("Hey, second!");
+});
+
+btnBlock.addEventListener('mouseover', event => {
+    if (event.target && event.target.tagName == 'BUTTON')
+        console.log("Hey, mouseover!");
+});
