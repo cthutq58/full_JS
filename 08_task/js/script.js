d62bfcaaa6be7d211741afc17c1333fbe39d1b9f
Ser <cthutq58@gmail.com>
2020-02-25 23:33:30 +0300

    08_task

--- 08_task/js/script.js
+++ 08_task/js/script.js
@@ -35,4 +35,80 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Timer
+
+    let deadline = '2020-03-21';
+    
+    // console.log(new Date(deadline));
+    // console.log(new Date(deadline).valueOf());
+    // console.log(Date.parse(deadline));
+    // console.log(new Date(deadline).toString());
+    // console.log(new Date(deadline).toISOString());
+    console.log(Date());
+    console.log(new Date());
+    console.log(new Date().toDateString());
+    console.log(Date().valueOf());
+    console.log(new Date().valueOf());
+    console.log(Date.parse(new Date()));
+    console.log(new Date().toString());
+    console.log(new Date().toISOString());
+    console.log(new Date().toJSON());
+    console.log(new Date().toTimeString());
+    // console.log(new Date().toISOString().match(/\d{4}-\d{2}-\d{2}/g));
+    console.log(new Date().toISOString().split('T'));
+    console.log(new Date().toJSON().split('T'));
+    // console.log(new Date().toISOString().match(/\d{2}:\d{2}:\d{2}/g));
+    // console.log(new Date().toISOString().match(/\d{2}/g));
+ 
+
+    function getTimeRemaining(endtime) {
+        // let t = Date.parse(endtime) - Date.parse(new Date()),
+        let t = Date.parse(endtime) - new Date().valueOf(),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            // function addZero(num){
+            //             if(num <= 9) {
+            //                 return '0' + num;
+            //             } else return num;
+            //         };
+
+            // hours.textContent = addZero(t.hours);
+            // minutes.textContent = addZero(t.minutes);
+            // seconds.textContent = addZero(t.seconds);
+
+            hours.textContent = ('0' + t.hours).slice(-2);
+            minutes.textContent = ('0' + t.minutes).slice(-2);
+            seconds.textContent = ('0' + t.seconds).slice(-2);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadline);   
 });
